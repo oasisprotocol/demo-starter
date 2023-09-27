98333bac0bd268e56a9d100a3e6ab3f931d62bc8
Matevž Jekovec <matevz@oasisprotocol.org>
2023-09-27 19:50:44 +0200

    frontend

---
+++ frontend/src/contracts.ts
@@ -0,0 +1,31 @@
+import { ethers } from 'ethers';
+import type { ComputedRef } from 'vue';
+import { computed } from 'vue';
+
+import { type MessageBox, MessageBox__factory } from '@oasisprotocol/demo-starter-backend';
+export type { MessageBox } from '@oasisprotocol/demo-starter-backend';
+
+import { useEthereumStore } from './stores/ethereum';
+
+const provider = new ethers.providers.JsonRpcProvider(
+  import.meta.env.VITE_WEB3_GATEWAY,
+  'any',
+);
+
+export const staticMessageBox = MessageBox__factory.connect(import.meta.env.VITE_MESSAGE_BOX_ADDR!, provider);
+
+export function useMessageBox(): ComputedRef<MessageBox> {
+  const eth = useEthereumStore();
+  const addr = import.meta.env.VITE_MESSAGE_BOX_ADDR!;
+  return computed(() => {
+    return MessageBox__factory.connect(addr, eth.signer ?? eth.provider);
+  });
+}
+
+export function useUnwrappedMessageBox(): ComputedRef<MessageBox> {
+  const eth = useEthereumStore();
+  const addr = import.meta.env.VITE_MESSAGE_BOX_ADDR!;
+  return computed(() => {
+    return MessageBox__factory.connect(addr, eth.unwrappedSigner ?? eth.unwrappedProvider);
+  });
+}
