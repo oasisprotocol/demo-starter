2da1d41953e3eb19e71485d1cda743bbe0d0ec4e
Matevž Jekovec <matevz@oasisprotocol.org>
2023-09-27 09:23:24 +0200

    backend

---
+++ backend/hardhat.config.ts
@@ -0,0 +1,115 @@
+//import '@oasisprotocol/sapphire-hardhat';
+import "@nomiclabs/hardhat-ethers"
+import { promises as fs } from 'fs';
+import path from 'path';
+
+import canonicalize from 'canonicalize';
+import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
+import { HardhatUserConfig, task } from 'hardhat/config';
+
+import '@typechain/hardhat';
+import 'hardhat-watcher';
+import 'solidity-coverage';
+
+const TASK_EXPORT_ABIS = 'export-abis';
+
+task(TASK_COMPILE, async (_args, hre, runSuper) => {
+  await runSuper();
+  await hre.run(TASK_EXPORT_ABIS);
+});
+
+task(TASK_EXPORT_ABIS, async (_args, hre) => {
+  const srcDir = path.basename(hre.config.paths.sources);
+  const outDir = path.join(hre.config.paths.root, 'abis');
+
+  const [artifactNames] = await Promise.all([
+    hre.artifacts.getAllFullyQualifiedNames(),
+    fs.mkdir(outDir, { recursive: true }),
+  ]);
+
+  await Promise.all(
+    artifactNames.map(async (fqn) => {
+      const { abi, contractName, sourceName } = await hre.artifacts.readArtifact(fqn);
+      if (abi.length === 0 || !sourceName.startsWith(srcDir) || contractName.endsWith('Test'))
+        return;
+      await fs.writeFile(`${path.join(outDir, contractName)}.json`, `${canonicalize(abi)}\n`);
+    }),
+  );
+});
+
+// Default DAO deployment, no permissions.
+task('deploy')
+  .setAction(async (args, hre) => {
+    await hre.run('compile');
+
+    const MessageBox = await hre.ethers.getContractFactory('MessageBox');
+    const messageBox = await MessageBox.deploy();
+    await messageBox.deployed();
+
+    console.log(`MessageBox address: ${messageBox.address}`);
+    return messageBox;
+});
+
+// Hardhat Node and sapphire-dev test mnemonic.
+const TEST_HDWALLET = {
+  mnemonic: "test test test test test test test test test test test junk",
+  path: "m/44'/60'/0'/0",
+  initialIndex: 0,
+  count: 20,
+  passphrase: "",
+};
+
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : TEST_HDWALLET;
+
+const config: HardhatUserConfig = {
+  networks: {
+    hardhat: { // https://hardhat.org/metamask-issue.html
+      chainId: 1337,
+    },
+    'sapphire': {
+      url: 'https://sapphire.oasis.io',
+      chainId: 0x5afe,
+      accounts,
+    },
+    'sapphire-testnet': {
+      url: 'https://testnet.sapphire.oasis.dev',
+      chainId: 0x5aff,
+      accounts,
+    },
+    'sapphire-localnet': { // docker run -it -p8545:8545 -p8546:8546 ghcr.io/oasisprotocol/sapphire-dev -test-mnemonic
+      url: 'http://localhost:8545',
+      chainId: 0x5afd,
+      accounts,
+    },
+  },
+  solidity: {
+    version: '0.8.16',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+    },
+  },
+  watcher: {
+    compile: {
+      tasks: ['compile'],
+      files: ['./contracts/'],
+    },
+    test: {
+      tasks: ['test'],
+      files: ['./contracts/', './test'],
+    },
+    coverage: {
+      tasks: ['coverage'],
+      files: ['./contracts/', './test'],
+    },
+  },
+  mocha: {
+    require: ['ts-node/register/files'],
+    timeout: 50_000,
+  },
+};
+
+export default config;
