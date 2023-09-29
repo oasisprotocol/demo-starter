95eeadaf717637f9e5b174542812bd1a6d7a0cd9
lubej <9722540+lubej@users.noreply.github.com>
2023-09-29 19:51:50 +0200

    UI improvement

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
-import type { MessageBox } from '../contracts';
-import {useMessageBox, useUnwrappedMessageBox} from '../contracts';
+import { useMessageBox, useUnwrappedMessageBox } from '../contracts';
 import { Network, useEthereumStore } from '../stores/ethereum';
+import { abbrAddr } from '@/utils/utils';
 import AppButton from '@/components/AppButton.vue';
 import MessageLoader from '@/components/MessageLoader.vue';
+import JazzIcon from '@/components/JazzIcon.vue';
+import { retry } from '@/utils/promise';
 
 const eth = useEthereumStore();
 const messageBox = useMessageBox();
@@ -18,11 +20,35 @@ const newMessage = ref('');
 const isLoading = ref(true);
 const isSettingMessage = ref(false);
 
-async function fetchMessage(): Promise<Record<string, string>> {
+interface Message {
+  message: string;
+  author: string;
+}
+
+async function fetchMessage(): Promise<Message> {
   const message = await uwMessageBox.value.message();
   const author = await uwMessageBox.value.author();
 
   return { message, author };
+}
+
+async function getMessage(): Promise<Message | null> {
+  let retrievedMessage: Message | null = null;
+
+  try {
+    retrievedMessage = await fetchMessage();
+    message.value = retrievedMessage.message;
+    author.value = retrievedMessage.author;
+
+    return retrievedMessage;
+  } catch (e) {
+    errors.value.push(`Failed to get message: ${e.message ?? JSON.stringify(e)}`);
+    console.error(e);
+  } finally {
+    isLoading.value = false;
+  }
+
+  return retrievedMessage;
 }
 
 async function setMessage(e: Event): Promise<void> {
@@ -32,14 +58,23 @@ async function setMessage(e: Event): Promise<void> {
   }
   e.preventDefault();
   try {
+    const newMessageValue = newMessage.value;
     errors.value.splice(0, errors.value.length);
-    isLoading.value = true;
-    await messageBox.value.setMessage(newMessage.value);
+    isSettingMessage.value = true;
+    await messageBox.value.setMessage(newMessageValue);
+
+    await retry<Promise<Message | null>>(getMessage, (retrievedMessage) => {
+      if (retrievedMessage?.message !== newMessageValue) {
+        throw new Error('Unable to determine if the new message has been correctly set!');
+      }
+
+      return retrievedMessage;
+    });
   } catch (e: any) {
     errors.value.push(`Failed to set message: ${e.message ?? JSON.stringify(e)}`);
     console.error(e);
   } finally {
-    isLoading.value = false;
+    isSettingMessage.value = false;
   }
 }
 
@@ -47,59 +82,95 @@ onMounted(async () => {
   await eth.connect();
   await eth.switchNetwork(Network.FromConfig);
 
-  await Promise.all([
-    fetchMessage().then((ret) => {
-      message.value = ret.message;
-      author.value = ret.author;
-      isLoading.value = false;
-    }),
-  ]);
+  getMessage();
 });
 </script>
 
 <template>
   <section class="pt-5">
-    <h2 class="capitalize">Demo Starter</h2>
+    <h1 class="capitalize text-2xl text-white font-bold mb-4">Demo starter</h1>
 
-    <p class="text-base">Message:</p>
-    <div v-if="!isLoading">
-      <p class="text-base">{{ message }}</p>
+    <h2 class="capitalize text-xl text-white font-bold mb-4">Active message</h2>
+    <p class="text-base mb-10 text-white">Current active message.</p>
+
+    <div class="message p-6 mb-6 rounded-xl border-2 border-gray-300" v-if="!isLoading">
+      <div class="flex items-center justify-between">
+        <h2 class="text-lg lg:text-lg m-0">{{ message }}</h2>
+        <div class="flex items-center flex-shrink-0">
+          <JazzIcon class="mr-2" :size="20" :address="author" />
+          <abbr :title="author" class="font-mono block no-underline">{{ abbrAddr(author) }}</abbr>
+        </div>
+      </div>
     </div>
-    <div v-else><MessageLoader/></div>
-    <p class="text-base">Author:</p>
-    <div v-if="!isLoading">
-      <p class="text-base">{{ author }}</p>
+    <div v-else>
+      <div class="message p-6 pt-4 mb-6 rounded-xl border-2 border-gray-300">
+        <MessageLoader />
+      </div>
     </div>
-    <div v-else><MessageLoader/></div>
+
+    <h2 class="capitalize text-xl text-white font-bold mb-4">Set message</h2>
+    <p class="text-base text-white mb-10">
+      Set your new message by filling the message field bellow.
+    </p>
 
     <form @submit="setMessage">
-      <label
+      <div class="form-group">
+        <input
+          type="text"
+          id="newMessageText"
+          class="peer"
+          placeholder=" "
+          v-model="newMessage"
+          required
+          :disabled="isSettingMessage"
+        />
+
+        <label
           for="newMessageText"
           class="peer-focus:text-primaryDark peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5"
-      >
-        New message:
-        <span class="text-red-500">*</span>
-      </label>
-      <input type="text" id="newMessageText" class="peer" placeholder=" " v-model="newMessage" required />
+        >
+          New message:
+          <span class="text-red-500">*</span>
+        </label>
+      </div>
 
       <AppButton type="submit" variant="primary" :disabled="isSettingMessage">
         <span v-if="isSettingMessage">Setting…</span>
         <span v-else>Set Message</span>
       </AppButton>
+
+      <div v-if="errors.length > 0" class="text-red-500 px-3 mt-5 rounded-xl-sm">
+        <span class="font-bold">Errors:</span>
+        <ul class="list-disc px-8">
+          <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+      </div>
     </form>
   </section>
 </template>
 
 <style scoped lang="postcss">
-form {
-  @apply text-center;
-}
-
 input {
-  @apply block my-4 p-1 mx-auto text-3xl text-center border border-gray-400 rounded-xl;
+  @apply block my-4 p-1 mx-auto text-3xl border border-gray-400 rounded-xl;
 }
 
-h2 {
-  @apply font-bold text-2xl my-2;
+.form-group {
+  @apply relative mb-6;
+}
+
+.form-group input,
+textarea {
+  @apply block rounded-xl py-6 px-5 w-full text-base text-black appearance-none focus:outline-none focus:ring-0 bg-white;
+}
+
+.form-group label {
+  @apply absolute text-base text-primaryDark duration-300 transform -translate-y-5 scale-75 top-6 z-10 origin-[0] left-5;
+}
+
+.message {
+  @apply bg-white rounded-xl border-primary;
+  border-width: 3px;
+  border-style: solid;
+  box-shadow: 0 7px 7px 0 rgba(0, 0, 0, 0.17);
 }
 </style>
