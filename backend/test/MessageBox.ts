203f50071ff05cfbfc7c2aace8c046225638ba33
Matevž Jekovec <matevz@oasisprotocol.org>
2023-09-29 10:19:47 +0200

    backend/contracts: setPrivateMessage, readPrivateMessage stub

--- backend/test/MessageBox.ts
+++ backend/test/MessageBox.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import {JsonRpcProvider} from "@ethersproject/providers";
 
 describe("MessageBox", function () {
   async function deployMessageBox() {
@@ -16,5 +17,36 @@ describe("MessageBox", function () {
 
     expect(await messageBox.message()).to.equal("hello world");
     expect(await messageBox.author()).to.equal(await ethers.provider.getSigner(0).getAddress());
+  });
+
+  it("Should send and read private message", async function () {
+    if ((await ethers.provider.getNetwork()).chainId != 1337) { // See https://github.com/oasisprotocol/sapphire-paratime/issues/197
+      this.skip();
+    }
+    const {messageBox} = await deployMessageBox();
+
+    await messageBox.setPrivateMessage((await ethers.getSigner(1)).address, "hello secret world");
+
+    const messageBox2 = messageBox.connect(await ethers.getSigner(1));
+
+    const [ privateMessage, privateAuthor ] = await messageBox2.readPrivateMessage();
+    expect(privateMessage).to.equal("hello secret world");
+    expect(privateAuthor).to.equal(await ethers.provider.getSigner(0).getAddress());
+  });
+
+  it("Should send and not unsigned read private message", async function () {
+    if ((await ethers.provider.getNetwork()).chainId == 1337) { // Requires nullified msg.sender.
+      this.skip();
+    }
+
+    const {messageBox} = await deployMessageBox();
+
+    await messageBox.setPrivateMessage((await ethers.getSigner(1)).address, "hello secret world");
+
+    const uwProvider = new JsonRpcProvider(ethers.provider.connection);
+    const messageBox2 = await ethers.getContractAt("MessageBox", messageBox.address,  new ethers.Wallet("0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", uwProvider));
+
+    const [ privateMessage ] = await messageBox2.readPrivateMessage();
+    expect(privateMessage).to.equal("");
   });
 });
