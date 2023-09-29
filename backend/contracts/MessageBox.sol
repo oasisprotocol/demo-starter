49df5c0574f4c0240c47a6108a1c47edfb13ecd0
Matevž Jekovec <matevz@oasisprotocol.org>
2023-09-29 10:19:47 +0200

    backend/contracts: setPrivateMessage, readPrivateMessage stub

--- backend/contracts/MessageBox.sol
+++ backend/contracts/MessageBox.sol
@@ -5,8 +5,19 @@ contract MessageBox {
     string public message;
     address public author;
 
+    mapping(address => string) privateMessage;
+    mapping(address => address) privateAuthor;
+
     function setMessage(string calldata in_message) external {
         message = in_message;
         author = msg.sender;
+    }
+
+    function setPrivateMessage(address in_recipient, string calldata in_message) external {
+        // TASK: Implement setting the private message and author.
+    }
+
+    function readPrivateMessage() external view returns (string memory, address) {
+        // TASK: Return the private message of the caller and its author.
     }
 }
