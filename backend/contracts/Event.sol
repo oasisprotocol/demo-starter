2ba8f1d260733fc65634c56f79c91bf4e4ffc62e
Giulio de Cadilhac <g.decadilhac@gmail.com>
2024-04-12 22:26:28 +0200

    basic contract mostly done but no oasis privacy yet

--- backend/contracts/Event.sol
+++ backend/contracts/Event.sol
@@ -2,29 +2,48 @@
 pragma solidity ^0.8.0;
 
 contract Event {
-    string public eventName
-    string public eventDescription
-    string public startDate
-    string public endDate
+    string public eventName;
+    string public eventDescription;
+    uint8 public startDate;
+    uint8 public endDate;
 
-    // interface mapping user
-    // - wallet address: 0x…
-    // - user name: deca12x
-    // - endorsements: [”string with endor1”, ”string with endor2”, ”string with endor3”…]
+    struct User {
+        string userName;
+        string[] endorsements;
+    }
 
-    constructor(string calldata _eventName, string calldata _eventDescription, string calldata _startDate, string calldata _endDate) {
+    mapping(address => User) public userRecords;
+
+    error UserAlreadyExists();
+    error EventNotStarted();
+    error DeadlineExpired();
+
+    constructor(string memory _eventName, string memory _eventDescription, uint8 memory _startDate, uint8 memory _endDate, string memory _userName) {
         eventName = _eventName
         eventDescription = _eventDescription
         startDate = _startDate
         endDate = _endDate
     }
 
-    function joinEvent() external {
-        // add user to event
+    function joinEvent(string memory userName) external {
+        if (block.timestamp > endDate) {
+            revert DeadlineExpired();
+        }
+        if (userRecords[msg.sender].userName != "") {
+            revert UserAlreadyExists();
+        }
+        User memory newUser = User(userName, []);
+        userRecords[msg.sender] = newUser;
     }
 
-    function endorse() external {
-        // add endorsement to user
+    function endorse(string memory endorsement) external {
+        if (block.timestamp < startDate) {
+            revert EventNotStarted();
+        }
+        if (block.timestamp > endDate) {
+            revert DeadlineExpired();
+        }
+        userRecords[msg.sender].endorsements.push(endorsement);
     }
 
     function deadline() external {
