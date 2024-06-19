8dbad371335aec2f70d614d610d62f3d83d2ec08
Matevž Jekovec <matevz@oasisprotocol.org>
2024-06-19 15:27:56 +0200

    backend: Add simple signature in place of signed query

--- backend/test/MessageBox.ts
+++ backend/test/MessageBox.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { config, ethers } from "hardhat";
+import "@nomicfoundation/hardhat-chai-matchers";
 
 describe("MessageBox", function () {
   async function deployMessageBox() {
@@ -14,7 +15,18 @@ describe("MessageBox", function () {
 
     await messageBox.setMessage("hello world");
 
-    expect(await messageBox.message()).to.equal("hello world");
+    // Check, if author is correctly set.
     expect(await messageBox.author()).to.equal(await (await ethers.provider.getSigner(0)).getAddress());
+
+    // Author should read a message.
+    const accounts = config.networks.hardhat.accounts;
+    const privKey = ethers.HDNodeWallet.fromMnemonic(ethers.Mnemonic.fromPhrase(accounts.mnemonic), accounts.path+'/0').privateKey;
+    const auth = (new ethers.SigningKey(privKey)).sign(ethers.keccak256(new Uint8Array()));
+    expect(await messageBox.message(auth)).to.equal("hello world");
+
+    // Anyone else trying to read the message should fail.
+    const privKey2 = ethers.HDNodeWallet.fromMnemonic(ethers.Mnemonic.fromPhrase(accounts.mnemonic), accounts.path+'/1').privateKey;
+    const auth2 = (new ethers.SigningKey(privKey2)).sign(ethers.keccak256(new Uint8Array()));
+    await expect(messageBox.message(auth2)).to.be.reverted;
   });
 });
