bff0baa0c0b6686664274c34f79bd50aa4b1d3c6
Matevž Jekovec <matevz@oasisprotocol.org>
2024-06-19 15:27:56 +0200

    backend: Add simple signature in place of signed query

--- backend/contracts/MessageBox.sol
+++ backend/contracts/MessageBox.sol
@@ -1,19 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+struct Sig {
+    uint8 v;
+    bytes32 r;
+    bytes32 s;
+}
+
 contract MessageBox {
     string private _message;
     address public author;
+
+    modifier _authorOnly(Sig calldata auth) {
+        address addr = ecrecover(keccak256(""), auth.v, auth.r, auth.s);
+        if (addr != author) {
+            revert("not allowed");
+        }
+        _;
+    }
 
     function setMessage(string calldata in_message) external {
         _message = in_message;
         author = msg.sender;
     }
 
-    function message() external view returns (string memory) {
-      if (msg.sender!=author) {
-        revert("not allowed");
-      }
+    function message(Sig calldata auth) external view _authorOnly(auth) returns (string memory) {
       return _message;
     }
 }
