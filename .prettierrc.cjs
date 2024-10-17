18ee3b3c00c66ef79f7f2cf0ef75946be3d330a5
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-10-17 07:09:07 +0200

    frontend: Add vite react boilerplate

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,29 +2,29 @@
 
 /** @type { import("prettier").ConfigType } */
 module.exports = {
-    printWidth: 110,
-    tabWidth: 2,
-    useTabs: false,
-    semi: false,
-    singleQuote: true,
-    trailingComma: 'es5',
-    arrowParens: 'avoid',
-    overrides: [
-        {
-            files: ['**/*.json'],
-            options: {
-                trailingComma: 'none',
-            },
-        },
-        {
-            files: '*.sol',
-            options: {
-                printWidth: 100,
-                tabWidth: 4,
-                useTabs: false,
-                singleQuote: false,
-                bracketSpacing: false,
-            }
-        }
-    ],
+  printWidth: 110,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  overrides: [
+    {
+      files: ['**/*.json'],
+      options: {
+        trailingComma: 'none',
+      },
+    },
+    {
+      files: '*.sol',
+      options: {
+        printWidth: 100,
+        tabWidth: 4,
+        useTabs: false,
+        singleQuote: false,
+        bracketSpacing: false,
+      },
+    },
+  ],
 }
