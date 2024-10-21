b990f945096ad352c51b6c05c84642e259b8f43f
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-10-21 08:54:51 +0200

    frontend: Base from existing dApps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
+import { execSync } from 'node:child_process'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    BUILD_COMMIT: JSON.stringify(execSync('git rev-parse HEAD').toString()),
+    BUILD_DATETIME: JSON.stringify(new Date().getTime()),
+  },
 })
